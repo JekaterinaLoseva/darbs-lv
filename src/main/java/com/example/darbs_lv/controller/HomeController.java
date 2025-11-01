465cbb10a03d3fb05e7b6b86854392b8badcee11
JekaerinaLoseva <loseva.jekaterina@gmail.com>
2025-11-01 20:51:42 +0200

    added HomeController and index page

---
+++ src/main/java/com/example/darbs_lv/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.darbs_lv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
